68c228889183388e93a525c3bd553074b67fe78e
Ivan <ivanrezv@icloud.com>
2022-07-12 15:11:06 +0300

    part 5 with half finish test models

--- clusterlist/views.py
+++ clusterlist/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'clusters/dashboard.html')
+    clusters = Cluster.objects.all()
+    total_clusters = clusters.count()
+    total_tickets = Cluster.objects.values('rds_link').distinct().count()
+    context = {'total_clusters': total_clusters, 'total_tickets': total_tickets}
+    return render(request, 'clusters/dashboard.html', context)
 
 
 def cluster_data(request):
-    return render(request, 'clusters/clusters.html')
-
+    clusters = Cluster.objects.all()
+    return render(request, 'clusters/clusters.html', {'clusters': clusters})
