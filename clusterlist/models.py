8ee6926c705ef9368431610a10147bda65e126c0
Ivan <ivanrezv@icloud.com>
2022-07-12 13:33:00 +0300

    part 5 finish test models

--- clusterlist/models.py
+++ clusterlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Cluster(models.Model):
+    system_name = models.CharField(max_length=100, null=True)
+    rds_link = models.CharField(max_length=100, null=True)
+    rds_status = models.CharField(max_length=100, null=True)
+    lt_namespace = models.CharField(max_length=100, null=True)
+    lt_cluster = models.CharField(max_length=100, null=True)
+    prod_namespace = models.CharField(max_length=100, null=True)
+    prod_cluster = models.CharField(max_length=100, null=True)
+    acc_requests = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.system_name
