ecc27163e21deef66191984532a33c3a9f16e63f
Ivan <ivanrezv@icloud.com>
2022-07-13 16:25:57 +0300

    pt9 finished, add dynamic info

--- portal/clusters/urls.py
+++ portal/clusters/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('product/', views.product),
     path('', views.home),
-    path('customer/', views.customer),
+    path('customer/<str:pk_test>/', views.customer),
 ]
 
