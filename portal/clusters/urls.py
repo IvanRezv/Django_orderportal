f3a99ff8ccbfdd497876de4ce91c37589f8ca805
Ivan <ivanrezv@icloud.com>
2022-07-13 16:55:12 +0300

    pt9.5 finished, add clickable buttons and links

--- portal/clusters/urls.py
+++ portal/clusters/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/', views.product),
-    path('', views.home),
-    path('customer/<str:pk_test>/', views.customer),
+    path('product/', views.product, name="products"),
+    path('', views.home, name="home"),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
 ]
 
