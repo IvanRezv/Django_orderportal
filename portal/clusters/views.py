8fc2a18ffdf138af4afb112b4269853bd9eaaf9f
Ivan <ivanrezv@icloud.com>
2022-07-13 16:08:34 +0300

    pt8 finished, add filter orders count

--- portal/clusters/views.py
+++ portal/clusters/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'clusters/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+    return render(request, 'clusters/dashboard.html', context)
 
 
 def product(request):
-    return render(request, 'clusters/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'clusters/products.html', {'products': products})
 
 
 def customer(request):
